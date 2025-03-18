784f0a600b1aca170841305b640567944b642e0d
Sonia Bhandari <sonia.bhandari@westcliff.edu>
2025-03-18 08:50:46 +0545

    second

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript Statement Example
+let welcomeMessage = "Welcome to FashionBliss.";
+console.log(welcomeMessage);
+
+// Function to Toggle the Hamburger Menu using ID
+function toggleMenu() {
+    let nav = document.querySelector(".nav-links");
+    nav.classList.toggle("show");
+}
+
+
+// JavaScript Output Example using ID
+function showMessage() {
+    document.getElementById("output").innerHTML = "FashionBliss is working!";
+}
